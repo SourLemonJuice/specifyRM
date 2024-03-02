6e06fa420f3d9c57ced8c6e0dbda4794c97a292a
SourLemonJuice <106436955+SourLemonJuice@users.noreply.github.com>
2024-03-02 18:32:35 +0800

    修复匹配时可能出现的bugs
    双引号，双引号，啊哈，双引号
    还有 dir.* 模式中，字符串截取位置超出原字符数造成的test报错

--- specifyRM_match.sh
+++ specifyRM_match.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # specifyRM_match v1
-
+# 
 # $1 == Match mode
 # $2 == Mode options
 # $3 == Path to be delete
-
+# 
 # return code:
 # 0: matched
 # 1: no match
-# 2: input error
-
+# 10: input error
+# 
 # mode list:
 # path.basename -- Match full path basename with $2
 # path.prefix -- Match full path prefix with $2
@@ -19,9 +19,9 @@
 # dir.suffix -- just match directory name suffix with $2
 function specifyRM_match
 {
-    [[ -z "$2" ]] && echo "specifyRM_match: ERROR mode options is NULL -> '$2'" && return 2
-    [[ -z "$3" ]] && echo "specifyRM_match: ERROR path parametric is NULL -> '$3'" && return 2
-    [[ -n "$4" ]] && echo "specifyRM_match: ERROR redundant parameter \$4 is '$4'" && return 2
+    [[ -z "$2" ]] && echo "specifyRM_match: ERROR mode options is NULL -> '$2'" && return 16
+    [[ -z "$3" ]] && echo "specifyRM_match: ERROR path parametric is NULL -> '$3'" && return 16
+    [[ -n "$4" ]] && echo "specifyRM_match: ERROR redundant parameter \$4 is '$4'" && return 16
 
     while true
     do
@@ -29,9 +29,10 @@ function specifyRM_match
         case "$1" in
         path.basename)
             # $(basename) will return file name so just match it
+            local basename="$(basename "$3")"
+
             return $(test \
-            $(basename "$3") \
-            = "$2")
+            "$basename" = "$2")
             ;;
         path.prefix)
             # bash has this syntax to intercept part of a string:
@@ -39,31 +40,43 @@ function specifyRM_match
             # by default "start_char_position" counting from the left side
             # 0 is the first character number of the string
             return $(test \
-            ${3:0:${#2}} = "$2")
+            "${3:0:${#2}}" = "$2")
             ;;
         path.suffix)
             # add a '-' "start_char_position" also can counting by right side
             # but there must be a space between ':' and '-'
             return $(test \
-            ${3: -${#2}} = "$2")
+            "${3: -${#2}}" = "$2")
             ;;
         dir.prefix)
             # now strings aren't variable...
-            # note: expr substr's start character number is 1
-            return $(test \
-            $(expr substr $(dirname "$3") 1 ${#2}) \
-            = "$2")
+            local dirName="$(dirname "$3")"
+
+            # preventing invalid character number from being used in string interception
+            # '-le' <--> '<='
+            if [[ ${#2} -le ${#dirName} ]]
+            then
+                # note: expr substr's start character number is 1
+                return $(test \
+                "$(expr substr "$dirName" 1 ${#2})" = "$2")
+            else
+                return 1
+            fi
             ;;
         dir.suffix)
-            # sorry, but it definitely working
-            local dirName=$(dirname "$3")
+            local dirName="$(dirname "$3")"
 
-            return $(test \
-            ${dirName: -${#2}} = "$2")
+            if [[ ${#2} -le ${#dirName} ]]
+            then
+                return $(test \
+                "${dirName: -${#2}}" = "$2")
+            else
+                return 1
+            fi
             ;;
         *)
             echo "specifyRM_match: Meow? Wrong mode name '$1'"
-            return 2
+            return 16
             ;;
         esac
     done
