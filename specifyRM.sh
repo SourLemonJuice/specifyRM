2287f8552ab232e5f17f944a536a6b6bb62a6468
SourLemonJuice <106436955+SourLemonJuice@users.noreply.github.com>
2024-03-02 18:38:32 +0800

    改进 specifyRM 中的语法

--- specifyRM.sh
+++ specifyRM.sh
@@ -4,15 +4,13 @@
 function specifyRM
 {
     echo "specifyRM function is just an example"
-    
-    specifyRM_match $@
 
-    if [[ $? -eq 0 ]]
-    then
+    specifyRM_match $@ && \
+    {
         echo "Soon to be delete(rm -r) '$3'"
         echo "sleeping 10sec [ctrl+c to exit]"
         sleep 10
         # real RM here
         # rm -r "$3"
-    fi
+    }
 }
